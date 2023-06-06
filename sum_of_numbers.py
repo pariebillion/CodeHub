cd8c702e1626e4d57f12de7b293956d64471b4d2
Paradise <135686413+pariebillion@users.noreply.github.com>
2023-06-06 06:13:25 +0100

    Added code for calculating the sum of two numbers
    Added code for calculating the sum of two numbers

---
+++ sum_of_numbers.py
@@ -0,0 +1,8 @@
+def calculate_sum(a, b):
+    return a + b
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+result = calculate_sum(num1, num2)
+print("The sum of", num1, "and", num2, "is", result)
